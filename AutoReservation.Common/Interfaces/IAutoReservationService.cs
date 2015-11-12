6f571a0f2ad884e3965fe485ad53e0458596478f
fabiankeller <fabian.keller91@gmail.com>
2015-11-12 11:26:46 +0100

    Added methods to the IAutoReservationService

--- AutoReservation.Common/Interfaces/IAutoReservationService.cs
+++ AutoReservation.Common/Interfaces/IAutoReservationService.cs
@@ -1,7 +1,38 @@
-﻿namespace AutoReservation.Common.Interfaces
+﻿using System.Collections.ObjectModel;
+using AutoReservation.Common.DataTransferObjects;
+
+namespace AutoReservation.Common.Interfaces
 {
     public interface IAutoReservationService
     {
-        
+        Collection<AutoDto> GetAllAutos();
+
+        AutoDto GetAuto(int autoId);
+
+        void AddAuto(AutoDto auto);
+
+        void UpdateAuto(AutoDto modified, AutoDto original);
+
+        void DeleteAuto(AutoDto auto);
+
+        Collection<KundeDto> GetAllKunde();
+
+        KundeDto GetKunde(int kundeId);
+
+        void AddKunde(KundeDto kunde);
+
+        void UpdateKunde(KundeDto modified, KundeDto original);
+
+        void DeleteKunde(KundeDto kunde);
+
+        Collection<ReservationDto> GetAllResevation();
+
+        ReservationDto GetReservation(int reservationNr);
+
+        void AddReserveration(ReservationDto reservation);
+
+        void UpdateReservation(ReservationDto modified, ReservationDto original);
+
+        void DeleteReservation(ReservationDto reservation);
     }
 }
